193c3439fd6c23f31387a4e8944a9b6bb44a17c7
galaedrik <78663482+galaedrik@users.noreply.github.com>
2021-04-23 17:31:01 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,9 @@ let doliButton;
 let nuroButton;
 let trainButton;
 let saveButton;
-var description, resultM;
+// var description, resultP;
+// import Doliprane from 'https://cdn.jsdelivr.net/gh/galaedrik/PWaI/model.json';
+// import NurofenFlash from 'https://cdn.jsdelivr.net/gh/galaedrik/PWaI/model.json';
 
 
 function modelReady() {
@@ -33,7 +35,71 @@ function setup() {
   mobilenet = ml5.featureExtractor('MobileNet', modelReady);
   classifier = mobilenet.classification(video, videoReady);
 // classifyDone;
+//  resultP=createP();
+//  description=createP();
 
+//   doliButton = createButton('Doliprane');
+//   doliButton.mousePressed(function() {
+//     classifier.addImage('Doliprane');
+//   });
+
+//   nuroButton = createButton('Nurofen');
+//   nuroButton.mousePressed(function() {
+//     classifier.addImage('Nurofen');
+//   });
+  
+//   trainButton = createButton('train');
+//   trainButton.mousePressed(function() {
+//     classifier.train(whileTraining);
+//   });
+
+//   saveButton = createButton('save');
+//   saveButton.mousePressed(function() {
+//     classifier.save();
+//   });
+}
+
+function draw() {
+  background(0);
+  image(video, 0, 0, 320, 240);
+  fill(255);
+  textSize(16);
+  text(label, 10, height - 10);
+}
+
+// function whileTraining(loss) {
+//   if (loss == null) {
+//     console.log('Training Complete');
+//     classifier.classify(gotResults);
+//   } else {
+//     console.log(loss);
+//   }
+// }
+
+function gotResults(error, result) {
+  if (error) {
+    console.error(error);
+  } else {
+    // updated to work with newer version of ml5
+    // label = result;
+    label = result[0].label;
+    classifier.classify(gotResults);
+    // resultP.html(result[0].label);
+    // description.html(result[0].label);
+    // classifyDone(result[0].label);
+  }
+} 
+
+// function classifyDone(label) {
+//   if (label  === Doliprane){
+//     resultP.html("This medicine is " + Doliprane);
+//     description.html("It is an analgesic (pain reliever) and an antipyretic (fever reducer). The active substance of this medicine is paracetamol. It is used to treat pain and/or fever, for example in case of headaches, flu, toothache, aches and pains, painful periods.");
+//   }
+//   else if (label  === NurofenFlash){
+//     resultP.html("This medicine is " + NurofenFlash);
+//     description.html("This medicine contains a non-steroidal anti-inflammatory drug: ibuprofen. It is indicated for adults and children weighing 30 kg or more (about 11-12 years) for the short-term treatment of fever and/or pain such as headaches, flu-like conditions, dental pain, aches and pains and painful periods.");
+//   }
+// }
 //   doliButton = createButton('Doliprane');
 //   doliButton.mousePressed(function() {
 //     classifier.addImage('Doliprane');
